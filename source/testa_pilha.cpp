2601b02bffbf113f03d066817c9438af196f7662
murilo-simio <murilo2110@hotmail.com>
2020-10-17 22:41:53 -0300

    teste

--- source/testa_pilha.cpp
+++ source/testa_pilha.cpp
@@ -10,5 +10,35 @@ TEST_CASE("Testando pilha", "[pilha]") {
     pilhaArray PILHA;
     PILHA.createStack(5);
     REQUIRE(PILHA.size() == 5);
+    REQUIRE(PILHA.isEmpty() == true);
+    REQUIRE(PILHA.isFull() == false);
 
+
+    SECTION("Testando metodo push") {
+        PILHA.push(2);
+        REQUIRE(PILHA.isEmpty() == false);
+    }
+
+    SECTION("Testando metodo pop") {
+        PILHA.pop();
+        REQUIRE(PILHA.isEmpty() == false);
+        for (int i = 0; i < 4; i++) {
+            PILHA.push(i);
+        }
+        REQUIRE(PILHA.size() == 5);
+        REQUIRE(PILHA.pop().pilhaElem == 3);
+        REQUIRE(PILHA.pop().pilhaElem == 2);
+        REQUIRE(PILHA.isFull() == false);
+        REQUIRE(PILHA.isEmpty() == false);
+    }
+
+    SECTION("Testando metodo top") {
+        PILHA.push(69);
+        REQUIRE(PILHA.top().pilhaElem == 69);
+    }
+
+    SECTION("Testando metodo setSize") {
+        PILHA.setSize(19);
+        REQUIRE(PILHA.size() == 19);
+    }
 }
